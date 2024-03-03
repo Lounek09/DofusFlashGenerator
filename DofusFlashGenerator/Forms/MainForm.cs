a39e37744333309783827f960f3cd2d4dfa8fae3
Lounek09 <lounek09@proton.me>
2024-03-03 17:25:58 +0100

    typo

--- DofusFlashGenerator/Forms/MainForm.cs
+++ DofusFlashGenerator/Forms/MainForm.cs
@@ -99,7 +99,7 @@ public sealed partial class MainForm : Form
         }
 
         mapKeys = mapKeys.Where(x => File.Exists($"{Constant.CLIENT_MAPS_FOLDER_PATH}/{x.GetSwfFileName()}"))
-            .OrderBy(x => x.Id).ToList();
+            .OrderBy(x => x.Id);
 
         if (mapKeys.SequenceEqual(_mapKeys))
         {
